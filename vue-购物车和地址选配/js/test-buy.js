1d7ee3cccea54dca5596386a9078c7ee0b8626df
Andy <565071984@qq.com>
2017-03-23 17:26:01 +0800

    购物车功能

--- vue-购物车和地址选配/js/test-buy.js
+++ vue-购物车和地址选配/js/test-buy.js
@@ -2,11 +2,15 @@ var vm = new Vue({
   el:'#app',
   data:{
     totalMoney:0,
-    productList:[]
+    productList:[],
+    checkAllFlag:false,
+    delFlag:false,
+    curProduct:''
   },
   mounted:function(){
     this.$nextTick(function(){
       vm.cartView();
+
     });
   },
   // 定义局部过滤器
@@ -19,18 +23,72 @@ var vm = new Vue({
   methods:{
     cartView: function(){
       // var _this = this;
-      this.$http.get("../data/cartData.json",{'id':123}).then(
+      axios.get("../data/cartData.json",{params:{
+        id:123
+      }}).then(
       //   function(res){
       //   _this.productList = res.body.result.list;
       //   _this.totalMoney = res.body.result.totalMoney;
       // }
       // es6写法
       res =>{
-        this.productList = res.body.result.list;
-        this.totalMoney = res.body.result.totalMoney;
+        this.productList = res.data.result.list;
+        // this.totalMoney = res.data.result.totalMoney;
       }
     );
+  },
+  changeMoney:function(product,way){
+    if(way>0){
+      product.productQuentity++;
+    }else{
+      product.productQuentity--;
+      if(product.productQuentity<2){
+        product.productQuentity=1;
+      }
     }
+    this.calcTotalPrice();
+  },
+  selectedProduct:function(item){
+    if(typeof item.checked == 'undefined'){
+      // 全局注册对象变量
+      // Vue.set(item,'checked',true);
+      // 局部注册对象变量
+      this.$set(item,'checked',true);
+    }else{
+      item.checked = !item.checked;
+    }
+    this.calcTotalPrice();
+  },
+  checkAll:function(flag){
+    this.checkAllFlag = flag;
+    var _this = this;
+    this.productList.forEach(function(item,index){
+      if(typeof item.checked == 'undefined'){
+        _this.$set(item,'checked',_this.checkAllFlag);
+      }else{
+        item.checked = _this.checkAllFlag;
+      }
+    });
+    this.calcTotalPrice();
+  },
+  calcTotalPrice:function(){
+    var _this = this;
+    this.totalMoney = 0;
+    this.productList.forEach(function(item,index){
+      if(item.checked){
+        _this.totalMoney += item.productPrice*item.productQuentity;
+      }
+    });
+  },
+  delConfirm:function(item){
+    this.delFlag = true;
+    this.curProduct = item;
+  },
+  delProduct:function(){
+    this.delFlag = false;
+    var index = this.productList.indexOf(this.curProduct);
+    this.productList.splice(index,1);
+  }
   }
 });
 // 定义全局过滤器
